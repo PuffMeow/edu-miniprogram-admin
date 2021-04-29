589b538ba034d72e1527360cdca5c4ab34646892
JiquanWang99 <951873902@qq.com>
2021-04-29 11:16:10 +0800

    feat: 增加mavon-editor配置信息

--- src/views/PublishArtical.vue
+++ src/views/PublishArtical.vue
@@ -42,7 +42,11 @@
         </el-checkbox-group>
       </el-form-item>
       <el-form-item label="文章内容">
-        <mavon-editor v-model="content" />
+        <mavon-editor
+          v-model="content"
+          :codeStyle="codeStyle"
+          :toolbars="markdownOption"
+        />
       </el-form-item>
 
       <el-form-item>
@@ -64,7 +68,39 @@ export default {
       photoUrl: '',
       typeName: '',
       title: '',
+      codeStyle: 'monokai-sublime',
       content: '',
+      markdownOption: {
+        bold: true, // 粗体
+        italic: true, // 斜体
+        header: true, // 标题
+        underline: true, // 下划线
+        strikethrough: true, // 中划线
+        mark: true, // 标记
+        superscript: true, // 上角标
+        subscript: true, // 下角标
+        quote: true, // 引用
+        ol: true, // 有序列表
+        ul: true, // 无序列表
+        link: true, // 链接
+        imagelink: true, // 图片链接
+        code: true, // code
+        table: true, // 表格
+        fullscreen: false, // 全屏编辑
+        readmodel: false, // 沉浸式阅读
+        htmlcode: false, // 展示html源码
+        help: true, // 帮助
+        undo: true, // 上一步
+        redo: true, // 下一步
+        trash: true, // 清空
+        save: false, // 保存（触发events中的save事件）
+        navigation: true, // 导航目录
+        alignleft: true, // 左对齐
+        aligncenter: true, // 居中
+        alignright: true, // 右对齐
+        subfield: false, // 单双栏模式
+        preview: true, // 预览
+      },
       tagList: [],
     }
   },
